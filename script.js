73f818bb38e73b2fb68f9c4b72d4bf6a11feab45
Eyuel <hypertimelink@gmail.com>
2023-12-02 09:56:30 -0800

    Add continent radio button and JS for flag filter

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// To filter by therir continent 
+document.addEventListener('DOMContentLoaded', function() {
+  const radioButtons = document.querySelectorAll('input[name="continent"]');
+  const flashcards = document.querySelectorAll('.flashcard');
+
+  radioButtons.forEach(function(radioButton) {
+      radioButton.addEventListener('change', function() {
+          const selectedContinent = this.value;
+
+          flashcards.forEach(function(flashcard) {
+              const flashcardContinent = flashcard.querySelector('img').getAttribute('data-continent');
+
+              if (selectedContinent === 'All' || selectedContinent === flashcardContinent) {
+                  flashcard.style.display = 'inline-block';
+              } else {
+                  flashcard.style.display = 'none';
+              }
+          });
+      });
+  });
+});
+
+// To display names when clicked
 const flashcards = document.querySelectorAll('.flashcard');
 
 flashcards.forEach(flashcard => {
